839d8a63ccfa4692976e17b1ea172ca3e666ceb7
JoseGeek78 <josemariarodriguez78@gmail.com>
2023-11-04 11:15:58 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import binarycode from './imagenes/binarycode-logo.jpg'
 
 function App() {
   return (
     <div className="App">
-      
-      
+      <div className='binarycode-logo-contenedor'>
+        <img
+          className='binarycode-logo' 
+          src={binarycode}
+          alt='Logo binary'/>
+      </div>
     </div>
   );
 }
