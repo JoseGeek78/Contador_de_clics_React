7cd9a4533baa324272f745ad81f625f7e04e2c51
JoseGeek78 <josemariarodriguez78@gmail.com>
2023-11-06 20:30:44 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import './App.css';
 import Boton from './componentes/Boton';
 import Contador from './componentes/Contador';
 import binarycode from './imagenes/binarycode-logo.jpg'
+import { useState } from 'react';
 
 function App() {
 
+  const [numClics, setNumClics] = useState(0);
+
   const manejarClic = () => {
-    console.log('Clic');
+    setNumClics(numClics + 1);
   }
 
   const reiniciarContador = () => {
